b2b1c78e6bb44306dba87581556c070659fcbadb
MaaritIMaenpaa <m.maenpaa@agro.au.dk>
2024-03-04 13:18:48 +0100

    Reading in data

---
+++ scripts/Session1.R
@@ -0,0 +1,10 @@
+setwd("C:/Users/au721810/Git Repositories/Stats101")
+
+results <- 1+2
+results
+
+data <- read.csv("example_data/Rexample_CO2.csv")
+
+data <- read.table("clipboard", header=T)
+
+data <- file.choose()
